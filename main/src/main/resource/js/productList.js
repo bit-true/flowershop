01dc71462bdfa0bea3ec7e81e79bc52b7ca59bad
Yoogh <yoogh95@gmail.com>
2021-07-12 19:42:47 +0900

    0712 img 파일명 변경, 부트스트랩 적용

---
+++ main/src/main/resource/js/productList.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  if($('.product-price').hasClass("price") === true) {
+    $('.regular-price').css({"font-size" : "13px", 
+                             "color" : "#999999",
+                             "text-decoration" : "line-through"})
+  } else {
+    $('.regular-price').css({"font-size" : "14px",
+                             "color" : "#181717"
+                            })
+  }
+})
