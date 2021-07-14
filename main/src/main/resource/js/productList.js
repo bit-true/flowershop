c680f97004e81bc76d0df37715fd89d8349ee197
Yoogh <yoogh95@gmail.com>
2021-07-14 20:23:31 +0900

    0714 더보기 빼고 완료

--- main/src/main/resource/js/productList.js
+++ main/src/main/resource/js/productList.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
   if($('.product-price').hasClass("price") === true) {
     $('.regular-price').css({"font-size" : "13px", 
                              "color" : "#999999",
@@ -9,4 +10,9 @@ $(document).ready(function(){
                              "color" : "#181717"
                             })
   }
+
+  $('.btn-more').click(function(){
+    $('.m_list').fadeIn(1000);
+  })
+  
 })
